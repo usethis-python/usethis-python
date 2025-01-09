445dbcb1af277fed85fa35f77a0b4d3ae10c442b
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-01-09 16:01:53 +1300

    Add failing test for parsing 3.14.0a3

--- tests/usethis/_integrations/uv/test_python.py
+++ tests/usethis/_integrations/uv/test_python.py
@@ -4,7 +4,10 @@ import pytest
 
 from usethis._integrations.pyproject.errors import PyProjectTOMLNotFoundError
 from usethis._integrations.pyproject.requires_python import MissingRequiresPythonError
-from usethis._integrations.uv.python import get_supported_major_python_versions
+from usethis._integrations.uv.python import (
+    _parse_python_version_from_uv_output,
+    get_supported_major_python_versions,
+)
 from usethis._test import change_cwd
 
 
@@ -57,3 +60,15 @@ name = "foo"
         # Act
         with change_cwd(tmp_path), pytest.raises(MissingRequiresPythonError):
             get_supported_major_python_versions()
+
+
+class TestParsePythonVersionFromUVOutput:
+    def test_alpha(self):
+        # Arrange
+        version = "cpython-3.14.0a3+freethreaded-linux-x86_64-gnu"
+
+        # Act
+        major_version = _parse_python_version_from_uv_output(version)
+
+        # Assert
+        assert major_version == "3.14.0a3"

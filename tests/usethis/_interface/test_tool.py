4a157bb8108c662e4a9196d7986887bc0f8d0def
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-01-16 11:03:58 +1300

    Add a benchmark and the codspeed integration (#201)
    * Add a benchmark and the codspeed integration
    
    * Only run the benchmarks on one strategy. Restore subprocess tests into test_tool.py file

--- tests/usethis/_interface/test_tool.py
+++ tests/usethis/_interface/test_tool.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 import pytest
+from typer.testing import CliRunner
 
 from usethis._config import usethis_config
+from usethis._interface.tool import app
 from usethis._subprocess import SubprocessFailedError, call_subprocess
 from usethis._test import change_cwd
 
@@ -54,3 +56,27 @@ class TestRuff:
                 call_subprocess(["usethis", "tool", "ruff"])
             else:
                 call_subprocess(["usethis", "tool", "ruff", "--offline"])
+
+
+class TestPytest:
+    def test_add(self, tmp_path: Path):
+        # Act
+        runner = CliRunner()
+        with change_cwd(tmp_path):
+            result = runner.invoke(app, ["pytest"])
+
+        # Assert
+        assert result.exit_code == 0
+
+
+@pytest.mark.benchmark
+def test_several_tools_add_and_remove(tmp_path: Path):
+    runner = CliRunner()
+    with change_cwd(tmp_path):
+        runner.invoke(app, ["pytest"])
+        runner.invoke(app, ["ruff"])
+        runner.invoke(app, ["deptry"])
+        runner.invoke(app, ["pre-commit"])
+        runner.invoke(app, ["ruff", "--remove"])
+        runner.invoke(app, ["pyproject-fmt"])
+        runner.invoke(app, ["pytest", "--remove"])

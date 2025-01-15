d227ecabc12537d69bd273596f40bba1df0ab01e
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-01-15 22:16:52 +1300

    Implement `usethis show sonarqube-config`

--- src/usethis/_core/show.py
+++ src/usethis/_core/show.py
@@ -1,7 +1,21 @@
+import sys
+
+from usethis._console import err_print
 from usethis._integrations.pyproject.name import get_name
+from usethis._integrations.sonarqube.config import get_sonar_project_properties
 from usethis._integrations.uv.init import ensure_pyproject_toml
+from usethis.errors import UsethisError
 
 
 def show_name() -> None:
     ensure_pyproject_toml()
     print(get_name())
+
+
+def show_sonarqube_config() -> None:
+    ensure_pyproject_toml()
+    try:
+        print(get_sonar_project_properties())
+    except UsethisError as err:
+        err_print(err)
+        sys.exit(1)
--- src/usethis/_interface/show.py
+++ src/usethis/_interface/show.py
@@ -1,7 +1,7 @@
 import typer
 
 from usethis._config import offline_opt, quiet_opt, usethis_config
-from usethis._core.show import show_name
+from usethis._core.show import show_name, show_sonarqube_config
 
 app = typer.Typer(help="Show information about the current project.")
 
@@ -13,3 +13,15 @@ def name(
 ) -> None:
     with usethis_config.set(offline=offline, quiet=quiet):
         show_name()
+
+
+@app.command(
+    name="sonarqube-config",
+    help="Show the sonar-projects.properties file for SonarQube.",
+)
+def sonarqube_config(
+    offline: bool = offline_opt,
+    quiet: bool = quiet_opt,
+) -> None:
+    with usethis_config.set(offline=offline, quiet=quiet):
+        show_sonarqube_config()

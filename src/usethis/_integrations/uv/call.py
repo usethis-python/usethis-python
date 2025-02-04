0f77f4c3938707e26020548b6e92d7a23db72f8c
godfather2327 <harshithsai94@gmail.com>
2025-02-03 22:49:36 -0800

    Modify call_uv_subprocess to use frozen

--- src/usethis/_integrations/uv/call.py
+++ src/usethis/_integrations/uv/call.py
@@ -1,7 +1,7 @@
 from usethis._integrations.pyproject.io_ import read_pyproject_toml_from_path
 from usethis._integrations.uv.errors import UVSubprocessFailedError
 from usethis._subprocess import SubprocessFailedError, call_subprocess
-
+from usethis._config import usethis_config
 
 def call_uv_subprocess(args: list[str]) -> str:
     """Run a subprocess using the uv command-line tool.
@@ -11,6 +11,9 @@ def call_uv_subprocess(args: list[str]) -> str:
     """
     read_pyproject_toml_from_path.cache_clear()
     try:
+        if usethis_config.frozen:
+            args.append("--frozen")
+            return call_subprocess(["uv", *args], check=False)
         return call_subprocess(["uv", *args])
     except SubprocessFailedError as err:
         raise UVSubprocessFailedError(err) from None

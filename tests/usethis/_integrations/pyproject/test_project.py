eebdbb58a4582b7bd487985c360c81b88c97a97e
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-02-08 11:11:53 +1300

    Add TestGetProjectDict for better coverage

---
+++ tests/usethis/_integrations/pyproject/test_project.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import pytest
+
+from usethis._integrations.pyproject.errors import (
+    PyProjectTOMLNotFoundError,
+    PyProjectTOMLProjectSectionError,
+)
+from usethis._integrations.pyproject.project import get_project_dict
+from usethis._test import change_cwd
+
+
+class TestGetProjectDict:
+    def test_no_file(self, tmp_path: Path):
+        with change_cwd(tmp_path), pytest.raises(PyProjectTOMLNotFoundError):
+            get_project_dict()
+
+    def test_empty_file(self, tmp_path: Path):
+        # Arrange
+        (tmp_path / "pyproject.toml").touch()
+
+        # Act, Assert
+        with change_cwd(tmp_path), pytest.raises(PyProjectTOMLProjectSectionError):
+            get_project_dict()
+
+    def test_missing_project_section(self, tmp_path: Path):
+        # Arrange
+        (tmp_path / "pyproject.toml").write_text("[tool.poetry]\n")
+
+        # Act, Assert
+        with change_cwd(tmp_path), pytest.raises(PyProjectTOMLProjectSectionError):
+            get_project_dict()
+
+    def test_invalid_project_section(self, tmp_path: Path):
+        # Arrange
+        (tmp_path / "pyproject.toml").write_text("project = ['bad']\n")
+
+        # Act, Assert
+        with change_cwd(tmp_path), pytest.raises(PyProjectTOMLProjectSectionError):
+            get_project_dict()
+
+    def test_valid_project_section(self, tmp_path: Path):
+        # Arrange
+        (tmp_path / "pyproject.toml").write_text(
+            "[project]\nname = 'foo'\ndescription = 'bar'\n"
+        )
+
+        # Act
+        with change_cwd(tmp_path):
+            project = get_project_dict()
+
+        # Assert
+        assert project == {"name": "foo", "description": "bar"}

f299ef365a66b11485d7da65b2cfe17d40dd7de9
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-02-20 11:49:58 +1300

    Add tests for ensure_pyproject_validity

--- src/usethis/_integrations/pyproject/valid.py
+++ src/usethis/_integrations/pyproject/valid.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 
 from tomlkit import TOMLDocument
-from tomlkit.container import Container
-from tomlkit.items import Table
+from tomlkit.items import Array, Table
 
 from usethis._console import tick_print
 from usethis._integrations.pyproject.io_ import (
@@ -17,9 +16,13 @@ def ensure_pyproject_validity():
 
     toml_document = read_pyproject_toml()
 
-    project = _ensure_project_section(toml_document)
-    _ensure_project_version(project)
-    _ensure_project_name(project)
+    try:
+        project = _ensure_project_section(toml_document)
+        _ensure_project_name(project)
+        _ensure_project_version(project)
+    except TypeError:
+        # Give up - the file is badly formatted. Let uv give a better error message.
+        return
 
     write_pyproject_toml(toml_document)
 
@@ -42,10 +45,9 @@ def _ensure_project_version(project: Table) -> None:
         return
     if "dynamic" in project:
         dynamic = project["dynamic"]
-        if not isinstance(dynamic, Container):
-            # Don't try to fix it if it's invalid
-            return
-        if "version" in dynamic:
+        if isinstance(dynamic, Array) and "version" in dynamic:
+            # N.B. if the dynamic section is in the wrong format then
+            # we will put 'version' in the project section.
             return
     tick_print("Setting project version to '0.1.0' in 'pyproject.toml'.")
     project["version"] = "0.1.0"
@@ -66,4 +68,7 @@ def _ensure_project_name(project: Table) -> None:
         name = "my_project"
 
     tick_print(f"Setting project name to '{name}' in 'pyproject.toml'.")
-    project["name"] = name
+    if project._value:
+        project._value._insert_at(0, "name", name)
+    else:
+        project["name"] = name

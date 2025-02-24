3f353e29450798cb87a066c7e1df5c3911914968
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-02-25 03:07:10 +1300

    Skip version bump test for pre-commit schema

--- tests/usethis/_integrations/pre_commit/test_pre_commit_schema.py
+++ tests/usethis/_integrations/pre_commit/test_pre_commit_schema.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -31,6 +32,10 @@ class TestSchemaJSON:
         # Compare the JSON
         assert local_schema_json == online_schema_json.replace("\r\n", "\n\n")
 
+    @pytest.mark.skipif(
+        os.getenv("CI"),
+        reason=".python-version re-pinned in CI for test matrix",
+    )
     def test_target_python_version(self):
         # If this test fails, we should bump the version in the command in schema.py
         assert Path(".python-version").read_text().startswith("3.10")

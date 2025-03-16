6c402e52a275fa0ec17039e48a6eba50547ed8e9
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-03-16 14:38:30 +1300

    Migrate tests into the correct file

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import pytest
 from git import InvalidGitRepositoryError, Repo
 
 
@@ -8,7 +7,6 @@ class PytestSuiteConfigurationError(Exception):
     pass
 
 
-@pytest.mark.skip(reason="In progress of a refactor")  # TODO re-enable
 def test_skeleton_matches():
     # If a tests/usethis/**/test_*.py exists, it should have a matching module named
     # src/usethis/**/*.py

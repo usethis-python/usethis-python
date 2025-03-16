884c0799910cb922f6bc6890e6f83355fbc70d3f
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-03-16 14:51:06 +1300

    Uncomment tests

--- tests/usethis/_integrations/file/pyproject_toml/test_pyproject_toml_io_.py
+++ tests/usethis/_integrations/file/pyproject_toml/test_pyproject_toml_io_.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import pytest
@@ -6,6 +7,8 @@ from tomlkit import TOMLDocument
 from usethis._integrations.file.pyproject_toml.errors import (
     PyprojectTOMLDecodeError,
     PyprojectTOMLNotFoundError,
+    PyprojectTOMLValueAlreadySetError,
+    PyprojectTOMLValueMissingError,
     UnexpectedPyprojectTOMLIOError,
     UnexpectedPyprojectTOMLOpenError,
 )
@@ -134,245 +137,240 @@ class TestPyprojectTOMLManager:
         # Assert
         assert result.value["tool"]["test"] == "value"
 
+    class TestSetItem:
+        def test_pyproject_does_not_exist(self, tmp_path: Path):
+            with (
+                change_cwd(tmp_path),
+                PyprojectTOMLManager(),
+                pytest.raises(PyprojectTOMLNotFoundError),
+            ):
+                PyprojectTOMLManager()[["tool", "usethis", "key"]]
 
-# TODO refactor these tests
-# class TestGetPyprojectValue:
-#     def test_pyproject_does_not_exist(self, tmp_path: Path):
-#         with (
-#             change_cwd(tmp_path),
-#             PyprojectTOMLManager(),
-#             pytest.raises(PyprojectTOMLNotFoundError),
-#         ):
-#             PyprojectTOMLManager()[["tool", "usethis", "key"]]
+        def test_key_does_not_exist(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key = "value"
+    """
+            )
 
-#     def test_key_does_not_exist(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key = "value"
-# """
-#         )
+            # Act, Assert
+            with (
+                change_cwd(tmp_path),
+                PyprojectTOMLManager(),
+                pytest.raises(KeyError),
+            ):
+                PyprojectTOMLManager()[["tool", "usethis", "key2"]]
 
-#         # Act, Assert
-#         with (
-#             change_cwd(tmp_path),
-#             PyprojectTOMLManager(),
-#             pytest.raises(KeyError),
-#         ):
-#             PyprojectTOMLManager()[["tool", "usethis", "key2"]]
+        def test_single_key(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key = "value"
+    """
+            )
 
-#     def test_single_key(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key = "value"
-# """
-#         )
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                value = PyprojectTOMLManager()[["tool", "usethis", "key"]]
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             value = PyprojectTOMLManager()[["tool", "usethis", "key"]]
+            # Assert
+            assert value == "value"
 
-#         # Assert
-#         assert value == "value"
+        def test_update_exists_ok(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key = "value1"
+    """
+            )
 
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                PyprojectTOMLManager()[["tool", "usethis", "key"]] = "value2"
 
-# class TestSetPyprojectValue:
-#     def test_empty(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").touch()
+            # Assert
+            assert (
+                (tmp_path / "pyproject.toml").read_text()
+                == """\
+    [tool.usethis]
+    key = "value2"
+    """
+            )
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             PyprojectTOMLManager().set_value(
-#                 keys=["tool", "usethis", "key"], value="value"
-#             )
+        def test_update_mixed_nested(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key1 = "value1"
+    key2 = "value2"
+    """
+            )
 
-#         # Assert
-#         assert (
-#             (tmp_path / "pyproject.toml").read_text()
-#             == """\
-# [tool.usethis]
-# key = "value"
-# """
-#         )
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                PyprojectTOMLManager()[["tool", "usethis", "key1"]] = "value3"
 
-#     def test_update_exists_ok(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key = "value1"
-# """
-#         )
+            # Assert
+            assert (
+                (tmp_path / "pyproject.toml").read_text()
+                == """\
+    [tool.usethis]
+    key1 = "value3"
+    key2 = "value2"
+    """
+            )
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             PyprojectTOMLManager()[["tool", "usethis", "key"]] = "value2"
+    class TestSetValue:
+        def test_empty(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").touch()
 
-#         # Assert
-#         assert (
-#             (tmp_path / "pyproject.toml").read_text()
-#             == """\
-# [tool.usethis]
-# key = "value2"
-# """
-#         )
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                PyprojectTOMLManager().set_value(
+                    keys=["tool", "usethis", "key"], value="value"
+                )
 
-#     def test_update_not_exists_ok(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key = "value1"
-# """
-#         )
+            # Assert
+            assert (
+                (tmp_path / "pyproject.toml").read_text()
+                == """\
+    [tool.usethis]
+    key = "value"
+    """
+            )
 
-#         # Act
-#         with (
-#             change_cwd(tmp_path),
-#             PyprojectTOMLManager(),
-#             pytest.raises(
-#                 PyprojectTOMLValueAlreadySetError,
-#                 match=re.escape(
-#                     "Configuration value 'tool.usethis.key' is already set."
-#                 ),
-#             ),
-#         ):
-#             PyprojectTOMLManager().set_value(
-#                 keys=["tool", "usethis", "key"], value="value2", exists_ok=False
-#             )
+        def test_update_not_exists_ok(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key = "value1"
+    """
+            )
 
-#     def test_update_mixed_nested(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key1 = "value1"
-# key2 = "value2"
-# """
-#         )
+            # Act
+            with (
+                change_cwd(tmp_path),
+                PyprojectTOMLManager(),
+                pytest.raises(
+                    PyprojectTOMLValueAlreadySetError,
+                    match=re.escape(
+                        "Configuration value 'tool.usethis.key' is already set."
+                    ),
+                ),
+            ):
+                PyprojectTOMLManager().set_value(
+                    keys=["tool", "usethis", "key"], value="value2", exists_ok=False
+                )
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             PyprojectTOMLManager()[["tool", "usethis", "key1"]] = "value3"
+    class TestDel:
+        def test_already_missing(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").touch()
 
-#         # Assert
-#         assert (
-#             (tmp_path / "pyproject.toml").read_text()
-#             == """\
-# [tool.usethis]
-# key1 = "value3"
-# key2 = "value2"
-# """
-#         )
+            # Act, Assert
+            with (
+                change_cwd(tmp_path),
+                PyprojectTOMLManager(),
+                pytest.raises(PyprojectTOMLValueMissingError),
+            ):
+                del PyprojectTOMLManager()[["tool", "usethis", "key"]]
 
+        def test_missing_pyproject(self, tmp_path: Path):
+            with (
+                change_cwd(tmp_path),
+                PyprojectTOMLManager(),
+                pytest.raises(PyprojectTOMLNotFoundError),
+            ):
+                del PyprojectTOMLManager()[["tool", "usethis", "key"]]
 
-# class TestRemovePyprojectValue:
-#     def test_already_missing(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").touch()
+        def test_single_key(self, tmp_path: Path):
+            """This checks the empty section cleanup."""
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key = "value"
+    """
+            )
 
-#         # Act, Assert
-#         with (
-#             change_cwd(tmp_path),
-#             PyprojectTOMLManager(),
-#             pytest.raises(PyprojectTOMLValueMissingError),
-#         ):
-#             del PyprojectTOMLManager()[["tool", "usethis", "key"]]
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                del PyprojectTOMLManager()[["tool", "usethis", "key"]]
 
-#     def test_missing_pyproject(self, tmp_path: Path):
-#         with (
-#             change_cwd(tmp_path),
-#             PyprojectTOMLManager(),
-#             pytest.raises(PyprojectTOMLNotFoundError),
-#         ):
-#             del PyprojectTOMLManager()[["tool", "usethis", "key"]]
+            # Assert
+            assert (tmp_path / "pyproject.toml").read_text() == ""
 
-#     def test_single_key(self, tmp_path: Path):
-#         """This checks the empty section cleanup."""
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key = "value"
-# """
-#         )
+        def test_multi_key(self, tmp_path: Path):
+            """This checks the empty section cleanup."""
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key1 = "value1"
+    key2 = "value2"
+    """
+            )
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             del PyprojectTOMLManager()[["tool", "usethis", "key"]]
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                del PyprojectTOMLManager()[["tool", "usethis", "key1"]]
 
-#         # Assert
-#         assert (tmp_path / "pyproject.toml").read_text() == ""
+            # Assert
+            assert (
+                (tmp_path / "pyproject.toml").read_text()
+                == """\
+    [tool.usethis]
+    key2 = "value2"
+    """
+            )
 
-#     def test_multi_key(self, tmp_path: Path):
-#         """This checks the empty section cleanup."""
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key1 = "value1"
-# key2 = "value2"
-# """
-#         )
+    class TestExtendList:
+        def test_empty(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").touch()
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             del PyprojectTOMLManager()[["tool", "usethis", "key1"]]
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                PyprojectTOMLManager().extend_list(
+                    keys=["tool", "usethis", "key"], values=["value"]
+                )
 
-#         # Assert
-#         assert (
-#             (tmp_path / "pyproject.toml").read_text()
-#             == """\
-# [tool.usethis]
-# key2 = "value2"
-# """
-#         )
+            # Assert
+            assert (
+                (tmp_path / "pyproject.toml").read_text()
+                == """\
+    [tool.usethis]
+    key = ["value"]
+    """
+            )
 
+        def test_add_one(self, tmp_path: Path):
+            # Arrange
+            (tmp_path / "pyproject.toml").write_text(
+                """\
+    [tool.usethis]
+    key = ["value1"]
+    """
+            )
 
-# class TestExtendPyprojectList:
-#     def test_empty(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").touch()
+            # Act
+            with change_cwd(tmp_path), PyprojectTOMLManager():
+                PyprojectTOMLManager().extend_list(
+                    keys=["tool", "usethis", "key"], values=["value2"]
+                )
 
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             PyprojectTOMLManager().extend_list(
-#                 keys=["tool", "usethis", "key"], values=["value"]
-#             )
-
-#         # Assert
-#         assert (
-#             (tmp_path / "pyproject.toml").read_text()
-#             == """\
-# [tool.usethis]
-# key = ["value"]
-# """
-#         )
-
-#     def test_add_one(self, tmp_path: Path):
-#         # Arrange
-#         (tmp_path / "pyproject.toml").write_text(
-#             """\
-# [tool.usethis]
-# key = ["value1"]
-# """
-#         )
-
-#         # Act
-#         with change_cwd(tmp_path), PyprojectTOMLManager():
-#             PyprojectTOMLManager().extend_list(
-#                 keys=["tool", "usethis", "key"], values=["value2"]
-#             )
-
-#         # Assert
-#         assert (
-#             (tmp_path / "pyproject.toml").read_text()
-#             == """\
-# [tool.usethis]
-# key = ["value1", "value2"]
-# """
-#         )
+            # Assert
+            assert (
+                (tmp_path / "pyproject.toml").read_text()
+                == """\
+    [tool.usethis]
+    key = ["value1", "value2"]
+    """
+            )

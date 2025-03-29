171ce41f2165be1c5111aa42f37ef61b3ea5f622
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-03-29 22:01:37 +1300

    Add `usethis docstyle` interface Improve the README

--- src/usethis/_integrations/pydantic/dump.py
+++ src/usethis/_integrations/pydantic/dump.py
@@ -35,6 +35,8 @@ def fancy_model_dump(
     """Like ``pydantic.model_dump`` but with bespoke formatting options.
 
     Args:
+        model: The model to dump. This can be a pydantic model or a representation of
+               the model (dict, list, etc.).
         reference: A representation to minimize the diff against. For example, if you
                    have a previous model_dump output and it contains default fields,
                    these will continue be included. If it omits default fields, these

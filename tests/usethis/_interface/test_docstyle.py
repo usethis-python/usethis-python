057c9d60f05dd27311e9ee950117c16e8585e7c0
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-04-04 16:21:49 +1300

    Reflect new interface in docstyle test

--- tests/usethis/_interface/test_docstyle.py
+++ tests/usethis/_interface/test_docstyle.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 
 import pytest
+import typer
 
 from usethis._interface.docstyle import docstyle
 from usethis._test import change_cwd
 
 
 class TestDocstyle:
-    def test_invalid_style(self):
-        with pytest.raises(ValueError, match="Invalid docstring style"):
+    def test_invalid_style(self, capfd: pytest.CaptureFixture[str]):
+        with pytest.raises(typer.Exit):
             docstyle("invalid_style")
+        out, err = capfd.readouterr()
+        assert "Invalid docstring style" in out
+        assert not err
 
     def test_google_runs(self, tmp_path: Path):
         with change_cwd(tmp_path):

ffddeee55fbcea2eb2f27ef157d7d0e5de9d3494
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-04-07 22:19:51 +1200

    Broaden exception handling for pyproject.toml project section being missing

--- src/usethis/_integrations/project/name.py
+++ src/usethis/_integrations/project/name.py
@@ -1,6 +1,6 @@
 from usethis._integrations.file.dir import get_project_name_from_dir
 from usethis._integrations.file.pyproject_toml.errors import (
-    PyprojectTOMLProjectNameError,
+    PyprojectTOMLProjectSectionError,
 )
 from usethis._integrations.file.pyproject_toml.name import get_name
 
@@ -9,5 +9,5 @@ def get_project_name() -> str:
     """The project name, from pyproject.toml if available or fallback to dir name."""
     try:
         return get_name()
-    except (PyprojectTOMLProjectNameError, FileNotFoundError):
+    except (PyprojectTOMLProjectSectionError, FileNotFoundError):
         return get_project_name_from_dir()

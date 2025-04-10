3022028b0d50f27b88ba34c7ec70fe73b71c7ded
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-04-11 07:49:18 +1200

    Add failing test

--- tests/usethis/_interface/test_docstyle.py
+++ tests/usethis/_interface/test_docstyle.py
@@ -36,3 +36,47 @@ class TestDocstyle:
 
         # Assert
         assert result.exit_code == 1, result.output
+
+    def test_pyproject_toml_success(self, tmp_path: Path):
+        # Arrange
+        valid_pyproject_toml = tmp_path / "pyproject.toml"
+        valid_pyproject_toml.touch()
+
+        # Act
+        with change_cwd(tmp_path):
+            runner = CliRunner()
+            result = runner.invoke(app, ["docstyle", "numpy"])
+
+        # Assert
+        assert result.exit_code == 0, result.output
+        assert (
+            "✔ Setting docstring style to 'numpy' in 'pyproject.toml'." in result.output
+        )
+
+        assert valid_pyproject_toml.exists()
+        content = valid_pyproject_toml.read_text()
+        assert "[tool.ruff.lint.pydocstyle]" in content
+
+    def test_adding_to_existing_file(self, tmp_path: Path):
+        # Arrange
+        existing_pyproject_toml = tmp_path / "pyproject.toml"
+        existing_pyproject_toml.write_text(
+            """\
+[project]
+name = "usethis"
+version = "0.1.0"
+
+[tool.ruff]
+lint.select = [ "A" ]
+"""
+        )
+
+        # Act
+        with change_cwd(tmp_path):
+            runner = CliRunner()
+            result = runner.invoke(app, ["docstyle", "pep257"])
+
+        # Assert
+        assert result.exit_code == 0, result.output
+        content = existing_pyproject_toml.read_text()
+        assert "[lint.pydocstyle]" not in content  # Wrong section name

260f09b4b7af176146fe8da9064b266bbe289b3e
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-05-19 18:03:46 +1200

    Pass test that removing pyproject.toml doesnt ever try to create pyproject.toml

--- src/usethis/_core/tool.py
+++ src/usethis/_core/tool.py
@@ -260,10 +260,9 @@ def use_pyproject_toml(*, remove: bool = False, how: bool = False) -> None:
         tool.print_how_to_use()
         return
 
-    ensure_pyproject_toml()
-
     if not remove:
         ensure_pyproject_toml()
+
         ensure_pyproject_validity()
         tool.print_how_to_use()
     else:

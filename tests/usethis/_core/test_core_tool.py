724ca276db2f2e83ad3dc667147646ef830839c1
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-06-02 14:36:35 +1200

    Test removing the linter from existing pre-commit config

--- tests/usethis/_core/test_core_tool.py
+++ tests/usethis/_core/test_core_tool.py
@@ -3105,6 +3105,48 @@ repos:
 """
             )
 
+        @pytest.mark.usefixtures("_vary_network_conn")
+        def test_remove_only_linter(
+            self, uv_init_repo_dir: Path, capfd: pytest.CaptureFixture[str]
+        ):
+            with change_cwd(uv_init_repo_dir), files_manager():
+                # Arrange
+                use_ruff()
+                use_pre_commit()
+                capfd.readouterr()
+
+                # Act
+                use_ruff(linter=True, formatter=False, remove=True)
+
+                # Assert
+                hook_names = get_hook_ids()
+
+            # 1. File exists
+            assert (uv_init_repo_dir / ".pre-commit-config.yaml").exists()
+
+            # 2. Hook is in the file
+            assert "ruff" not in hook_names
+            assert "ruff-format" in hook_names
+
+            # 3. Test file contents
+            assert (uv_init_repo_dir / ".pre-commit-config.yaml").read_text() == (
+                """\
+repos:
+  - repo: local
+    hooks:
+      - id: ruff-format
+        name: ruff-format
+        types_or:
+          - python
+          - pyi
+          - jupyter
+        always_run: true
+        entry: uv run --frozen --offline ruff format --force-exclude
+        language: system
+        require_serial: true
+"""
+            )
+
     class TestConfig:
         def test_removed(self, uv_init_dir: Path):
             # Arrange

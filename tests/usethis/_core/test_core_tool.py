a1f9b2ab4a63cc59ad686bfada58b185f668ef54
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-06-02 14:44:08 +1200

    Test removing the Ruff linter pre-commit when the lint config doesn't exist in a TOML file

--- tests/usethis/_core/test_core_tool.py
+++ tests/usethis/_core/test_core_tool.py
@@ -3147,6 +3147,74 @@ repos:
 """
             )
 
+        @pytest.mark.usefixtures("_vary_network_conn")
+        def test_remove_only_linter_yaml_only(
+            self, uv_init_repo_dir: Path, capfd: pytest.CaptureFixture[str]
+        ):
+            # Arrange
+            (uv_init_repo_dir / ".pre-commit-config.yaml").write_text("""\
+repos:
+  - repo: local
+    hooks:
+      - id: ruff
+        name: ruff
+        types_or:
+          - python
+          - pyi
+          - jupyter
+        always_run: true
+        entry: uv run --frozen --offline ruff check --fix --force-exclude
+        language: system
+        require_serial: true
+  - repo: local
+    hooks:
+      - id: ruff-format
+        name: ruff-format
+        types_or:
+          - python
+          - pyi
+          - jupyter
+        always_run: true
+        entry: uv run --frozen --offline ruff format --force-exclude
+        language: system
+        require_serial: true
+""")
+
+            with change_cwd(uv_init_repo_dir), files_manager():
+                capfd.readouterr()
+
+                # Act
+                use_ruff(linter=True, formatter=False, remove=True)
+
+                # Assert
+                hook_names = get_hook_ids()
+
+            # 1. File exists
+            assert (uv_init_repo_dir / ".pre-commit-config.yaml").exists()
+
+            # 2. Hook is in the file
+            assert "ruff" not in hook_names
+            assert "ruff-format" in hook_names
+
+            # 3. Test file contents
+            assert (uv_init_repo_dir / ".pre-commit-config.yaml").read_text() == (
+                """\
+repos:
+  - repo: local
+    hooks:
+      - id: ruff-format
+        name: ruff-format
+        types_or:
+          - python
+          - pyi
+          - jupyter
+        always_run: true
+        entry: uv run --frozen --offline ruff format --force-exclude
+        language: system
+        require_serial: true
+"""
+            )
+
     class TestConfig:
         def test_removed(self, uv_init_dir: Path):
             # Arrange

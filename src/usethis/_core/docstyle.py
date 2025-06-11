272fd27b812960fdc635cac8c77a2af1b932a0f8
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-06-12 06:24:44 +1200

    Use docstyle Enum at interface level

--- src/usethis/_core/docstyle.py
+++ src/usethis/_core/docstyle.py
@@ -1,19 +1,20 @@
-from typing import Literal
+from enum import Enum
 
 from usethis._core.tool import use_ruff
 from usethis._tool.impl.ruff import RuffTool
-from usethis.errors import UsethisError
 
 
-class UnknownDocstringStyleError(ValueError, UsethisError):
-    """Exception raised for unknown docstring styles."""
+class DocStyleEnum(Enum):
+    numpy = "numpy"
+    google = "google"
+    pep257 = "pep257"
 
 
-def use_docstyle(style: Literal["numpy", "google", "pep257"]) -> None:
+def use_docstyle(style: DocStyleEnum) -> None:
     if not RuffTool().is_used():
         use_ruff(minimal=True)
 
-    RuffTool().set_docstyle(style)
+    RuffTool().set_docstyle(style.value)
 
     if not RuffTool()._are_pydocstyle_rules_selected():
         RuffTool().select_rules(["D2", "D3", "D4"])

b6a10adbcd46d59acf7daea78f7d04d803e004fe
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-06-30 18:29:51 +1200

    Add tests for `is_uv_available` and for `get_backend`

--- src/usethis/_integrations/backend/uv/available.py
+++ src/usethis/_integrations/backend/uv/available.py
@@ -5,7 +5,8 @@ from usethis._integrations.backend.uv.errors import UVSubprocessFailedError
 def is_uv_available() -> bool:
     """Check if the `uv` command is available in the current environment."""
     try:
-        call_uv_subprocess(["--version"], change_toml=True)
-        return True
+        call_uv_subprocess(["--version"], change_toml=False)
     except UVSubprocessFailedError:
         return False
+
+    return True

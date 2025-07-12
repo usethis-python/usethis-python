2f4003329ead18ea729e574f7a23738a7d30e99c
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-07-12 16:52:28 +1200

    Support `--offline` and `--frozen` in `usethis docstyle` (#846)

--- src/usethis/_interface/docstyle.py
+++ src/usethis/_interface/docstyle.py
@@ -3,14 +3,16 @@ from __future__ import annotations
 import typer
 
 from usethis._core.enums.docstyle import DocStyleEnum
-from usethis._options import quiet_opt
+from usethis._options import frozen_opt, offline_opt, quiet_opt
 
 
 def docstyle(
     style: DocStyleEnum = typer.Argument(
         default="google", help="Docstring style to enforce."
     ),
+    offline: bool = offline_opt,
     quiet: bool = quiet_opt,
+    frozen: bool = frozen_opt,
 ) -> None:
     from usethis._config import usethis_config
     from usethis._config_file import files_manager
@@ -20,7 +22,10 @@ def docstyle(
 
     assert isinstance(style, DocStyleEnum)
 
-    with usethis_config.set(quiet=quiet), files_manager():
+    with (
+        usethis_config.set(offline=offline, quiet=quiet, frozen=frozen),
+        files_manager(),
+    ):
         try:
             use_docstyle(style)
         except UsethisError as err:

609e670645d3c46347b66e58f96fc21e3f3c74c7
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-08-01 18:42:26 +1200

    rework `is_uv_used` for robustness

--- src/usethis/_integrations/backend/uv/used.py
+++ src/usethis/_integrations/backend/uv/used.py
@@ -3,8 +3,13 @@ from usethis._integrations.file.pyproject_toml.io_ import PyprojectTOMLManager
 
 
 def is_uv_used() -> bool:
+    pyproject_toml_manager = PyprojectTOMLManager()
+
     return (
-        ["tool", "uv"] in PyprojectTOMLManager()
-        or (usethis_config.cpd() / "uv.lock").exists()
+        (usethis_config.cpd() / "uv.lock").exists()
         or (usethis_config.cpd() / "uv.toml").exists()
+        or (
+            pyproject_toml_manager.path.exists()
+            and ["tool", "uv"] in pyproject_toml_manager
+        )
     )

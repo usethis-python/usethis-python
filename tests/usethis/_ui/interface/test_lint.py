285a0801cb3aa978d25ac6c019854d179a85bb1f
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-10-14 05:18:59 +1300

    Continue to align tests with new expectations

--- tests/usethis/_ui/interface/test_lint.py
+++ tests/usethis/_ui/interface/test_lint.py
@@ -61,11 +61,12 @@ class TestLint:
         assert result.exit_code == 0, result.output
         assert (tmp_path / "pyproject.toml").exists()
         assert result.output.replace("\n", "") == (
+            "☐ Add the dev dependency 'deptry'.\n"
+            "✔ Adding deptry config to 'pyproject.toml'.\n"
+            "☐ Run 'deptry .' to run deptry.\n"
             "☐ Add the dev dependency 'ruff'.\n"
             "✔ Adding Ruff config to 'pyproject.toml'.\n"
             "✔ Selecting Ruff rules 'A', 'C4', 'E4', 'E7', 'E9', 'F', 'FLY', 'FURB', 'I', 'PLE', 'PLR', 'RUF', 'SIM', 'UP' in 'pyproject.toml'.\n"
             "✔ Ignoring Ruff rules 'PLR2004', 'SIM108' in 'pyproject.toml'.\n"
             "☐ Run 'ruff check --fix' to run the Ruff linter with autofixes.\n"
-            "☐ Add the dev dependency 'deptry'.\n"
-            "☐ Run 'deptry .' to run deptry.\n"
         ).replace("\n", "")

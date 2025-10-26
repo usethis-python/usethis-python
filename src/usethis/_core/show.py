f0bee17fbf545fdb2ff5acd769969cd4aa716fa7
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-10-27 10:20:39 +1300

    Don't create pyproject.toml if it does not exist when showing SonarQube config

--- src/usethis/_core/show.py
+++ src/usethis/_core/show.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from usethis._config import usethis_config
 from usethis._console import plain_print
-from usethis._init import ensure_pyproject_toml
 from usethis._integrations.project.name import get_project_name
 from usethis._integrations.sonarqube.config import get_sonar_project_properties
 
@@ -12,6 +10,4 @@ def show_name() -> None:
 
 
 def show_sonarqube_config() -> None:
-    with usethis_config.set(quiet=True):
-        ensure_pyproject_toml()
     plain_print(get_sonar_project_properties())

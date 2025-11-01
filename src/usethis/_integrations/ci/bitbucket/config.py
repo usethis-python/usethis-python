6150d438b3b494b07da1eff42fd5c4a73f2433ca
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-11-01 15:01:03 +1300

    Fix pluralization of `*_bitbucket_pipelines_config` functions

--- src/usethis/_integrations/ci/bitbucket/config.py
+++ src/usethis/_integrations/ci/bitbucket/config.py
@@ -3,7 +3,7 @@ from usethis._console import tick_print
 from usethis._integrations.ci.bitbucket.steps import add_placeholder_step_in_default
 
 
-def add_bitbucket_pipeline_config(report_placeholder: bool = True) -> None:
+def add_bitbucket_pipelines_config(report_placeholder: bool = True) -> None:
     """Add a Bitbucket pipeline configuration.
 
     Note that the pipeline is empty and will need steps added to it to run successfully.
@@ -15,7 +15,7 @@ def add_bitbucket_pipeline_config(report_placeholder: bool = True) -> None:
     add_placeholder_step_in_default(report_placeholder=report_placeholder)
 
 
-def remove_bitbucket_pipeline_config() -> None:
+def remove_bitbucket_pipelines_config() -> None:
     if not (usethis_config.cpd() / "bitbucket-pipelines.yml").exists():
         # Early exit; the file already doesn't exist
         return

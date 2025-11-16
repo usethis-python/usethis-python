ba23426f32f615073aad9a1bc0c29893be5b57e9
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-11-16 22:31:30 +1300

    Fix mistake in merge conflict resolution

--- tests/docs/test_readme.py
+++ tests/docs/test_readme.py
@@ -38,7 +38,6 @@ def test_assemble_readme_from_docs(usethis_dev_dir: Path):
             "](https://usethis.readthedocs.io/en/stable/frameworks)",
         )
     )
-    parts.append(_get_doc_file(usethis_dev_dir / "docs" / "similar-projects.md"))
     parts.append(_get_doc_file(usethis_dev_dir / "docs" / "about-license.md"))
 
     content = (

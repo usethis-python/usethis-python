72a4cb1eba00421fff3d6b9c2d4811d3eabf16b3
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-12-29 11:23:13 +1300

    Create a `get_pre_commit_version` function (#1240)
    * Create a `get_pre_commit_version` function
    
    This uses the declared minimum version in the config YAML file; otherwise a hard-coded recent version.
    
    A more complex heuristic would be easy (e.g. you could look at the declared dependencies) but it seems better to keep things simple for now.
    
    * Fix docstring reference

--- src/usethis/_integrations/pre_commit/version.py
+++ src/usethis/_integrations/pre_commit/version.py
@@ -1,6 +1,20 @@
 from usethis._config import usethis_config
 from usethis._integrations.pre_commit.io_ import read_pre_commit_config_yaml
 
+PRE_COMMIT_VERSION = "4.5.1"
+
+
+def get_pre_commit_version() -> str:
+    """Get an inferred pre-commit version for usethis to target.
+
+    If no version can be inferred, a hard-coded version is used, corresponding to a
+    recent release (see `PRE_COMMIT_VERSION`).
+    """
+    version = get_minimum_pre_commit_version()
+    if version is not None:
+        return version
+    return PRE_COMMIT_VERSION
+
 
 def get_minimum_pre_commit_version() -> str | None:
     """Get the declared minimum supported pre-commit version from the configuration.
--- tests/usethis/_tool/impl/test_pre_commit.py
+++ tests/usethis/_tool/impl/test_pre_commit.py
@@ -8,6 +8,7 @@ from usethis._config_file import files_manager
 from usethis._integrations.ci.github.errors import GitHubTagError
 from usethis._integrations.ci.github.tags import get_github_latest_tag
 from usethis._integrations.pre_commit.schema import UriRepo
+from usethis._integrations.pre_commit.version import PRE_COMMIT_VERSION
 from usethis._test import change_cwd
 from usethis._tool.impl.pre_commit import PreCommitTool
 
@@ -47,6 +48,27 @@ class TestPreCommitTool:
         try:
             assert repo.rev == get_github_latest_tag(
                 owner="tsvikas", repo="sync-with-uv"
+            )
+        except GitHubTagError as err:
+            if (
+                usethis_config.offline
+                or "rate limit exceeded for url" in str(err)
+                or "Read timed out." in str(err)
+            ):
+                pytest.skip(
+                    "Failed to fetch GitHub tags (connection issues); skipping test"
+                )
+            raise err
+
+    @pytest.mark.usefixtures("_vary_network_conn")
+    def test_latest_version(self):
+        if os.getenv("CI"):
+            pytest.skip("Avoid flaky pipelines by testing version bumps manually")
+
+        try:
+            assert (
+                get_github_latest_tag(owner="pre-commit", repo="pre-commit")
+                == f"v{PRE_COMMIT_VERSION}"
             )
         except GitHubTagError as err:
             if (

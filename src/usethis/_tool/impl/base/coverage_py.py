a540ddd556e96fa0d15ba6daa9d590ff10ab83ca
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2026-03-21 09:53:15 +1300

    Remove import cycles (#1436)

--- src/usethis/_tool/impl/base/coverage_py.py
+++ src/usethis/_tool/impl/base/coverage_py.py
@@ -8,7 +8,9 @@ from usethis._backend.dispatch import get_backend
 from usethis._backend.uv.detect import is_uv_used
 from usethis._console import how_print
 from usethis._tool.base import Tool
+from usethis._tool.heuristics import is_likely_used
 from usethis._tool.impl.spec.coverage_py import CoveragePyToolSpec
+from usethis._tool.impl.spec.pytest import PytestToolSpec
 from usethis._types.backend import BackendEnum
 from usethis._types.deps import Dependency
 
@@ -16,24 +18,16 @@ from usethis._types.deps import Dependency
 class CoveragePyTool(CoveragePyToolSpec, Tool):
     @final
     def test_deps(self, *, unconditional: bool = False) -> list[Dependency]:
-        from usethis._tool.impl.base.pytest import (  # to avoid circularity;  # noqa: PLC0415
-            PytestTool,
-        )
-
         deps = [Dependency(name="coverage", extras=frozenset({"toml"}))]
-        if unconditional or PytestTool().is_used():
+        if unconditional or is_likely_used(PytestToolSpec()):
             deps += [Dependency(name="pytest-cov")]
         return deps
 
     @final
     def print_how_to_use(self) -> None:
-        from usethis._tool.impl.base.pytest import (  # to avoid circularity;  # noqa: PLC0415
-            PytestTool,
-        )
-
         backend = get_backend()
 
-        if PytestTool().is_used():
+        if is_likely_used(PytestToolSpec()):
             if backend is BackendEnum.uv and is_uv_used():
                 how_print(
                     f"Run 'uv run pytest --cov' to run your tests with {self.name}."

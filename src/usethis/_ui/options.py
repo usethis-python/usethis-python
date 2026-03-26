0601d5a7a7329ced90b3d74ba5289af1b61ac00b
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2026-03-27 11:23:40 +1300

    Tweak wording in src/usethis/_ui/options.py

--- src/usethis/_ui/options.py
+++ src/usethis/_ui/options.py
@@ -30,7 +30,7 @@ backend_opt = typer.Option(
     BACKEND_DEFAULT, "--backend", help="Package manager backend to use."
 )
 no_hook_opt = typer.Option(
-    False, "--no-hook", help="Don't add or remove git hook configuration."
+    False, "--no-hook", help="Don't add or modify git hook configuration, e.g. pre-commit"
 )
 
 # author command options
